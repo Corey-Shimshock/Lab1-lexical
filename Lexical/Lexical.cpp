b208a4fbc52db9e6ae201895fca45118cf0e74e9
Manraj Singh <65259711+manraj23s@users.noreply.github.com>
2021-02-07 13:43:33 -0500

    Update Lexical.cpp with comments

--- Lexical/Lexical.cpp
+++ Lexical/Lexical.cpp
@@ -5,31 +5,37 @@
 #include "Text.h"
 using namespace std;
 
+///Preconditions: The text .dat file must be readable from istream. 
+///Postconditions: The individual delimited strings (tokens) are extracted and outputted as a count from ostream.
+///Input: progsamp.dat text file is opened and read
+///Output: The tokens are outputted one-by-one in a line and counted.
 int main() {
-	//set counter
+	//set counter to start at 1
 	int counter = 1;
-	//text token to read each token
-	Text token;
 
+	//text token to read each delimited string
+	Text token;
+	
+	//open text file
 	ifstream progFile;
 	progFile.open("progsampdat.txt");
-	//file path
+	
+	//if statement to validate if file is open
 	if (progFile.is_open()) {
 
 		while (!progFile.eof()) {
-
+			//takes in tokens
 			progFile >> token;
-
+			//if the file does not end, continue extracting tokens
 			if (progFile.eof()) break;
-			//counter to count each toekn
+			//counter counts each token and increments
 			cout << counter << ": [" << token << "]" << endl;
-
 			counter += 1;
-
 		}
+		//close text file
 		progFile.close();
-
 	}
+	//if file not opened, then throw error message
 	else {
 		cout << "Error: Couldn't read 'progsamp dat.txt'" << endl;
 
